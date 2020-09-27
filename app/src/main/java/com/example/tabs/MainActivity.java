ac7f89da0b08ec61cfd681dbd59a25c670e0afcb
wladimir Rocha <wladimir.rocha@equilibrio.agency>
2020-09-27 16:20:46 -0400

    first commit, create and inflate actionBar

---
+++ app/src/main/java/com/example/tabs/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.tabs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setSupportActionBar((androidx.appcompat.widget.Toolbar) findViewById(R.id.myToolbar));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
